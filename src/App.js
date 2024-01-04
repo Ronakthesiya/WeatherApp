0e91cd5ae8e9384d5d659ec62c7ff482984c3bec
Ronakthesiya <127646842+Ronakthesiya@users.noreply.github.com>
2024-01-04 20:59:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React,{useState} from 'react'
+import { WeatherApp } from "./Components/WeatherApp/WeatherApp";
+
+const App = () => {
+    
+    return (
+        <div>
+            <WeatherApp/>
+        </div>
+    );
+};
+
+export default App;
